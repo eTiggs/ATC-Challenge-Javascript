a4a1f8bdb2c1875a8305c7e243122d4eb18280da
eTiggs <edunwo01@student.bbk.ac.uk>
2024-04-23 18:36:43 +0100

    Failing test - getPlanes

---
+++ spec/airport.spec.js
@@ -0,0 +1,38 @@
+import Airport from '../src/airport.js';
+import Plane from '../src/plane.js';
+
+let airport;
+let plane;
+let actual, result;
+
+const afterEach = () => {
+    airport = undefined;
+    plane = undefined;
+}
+
+
+console.log(`Test 1`);
+console.log(`==================`);
+console.log(
+    `Check if the plane can land at the airport`
+);
+// Arrange
+let testPlane = new Plane('EthanAir 1');
+let testAirport = new Airport();
+let expected = airport.getPlanes.length + 1;
+
+// Act
+testAirport.landPlane(testPlane);
+
+actual = testAirport.getPlanes.length;
+// Assert
+
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
