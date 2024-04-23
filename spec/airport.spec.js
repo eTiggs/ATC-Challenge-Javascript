73f6a8a206696d7e0f40386c57b32eb4ab2f0f17
eTiggs <edunwo01@student.bbk.ac.uk>
2024-04-23 18:49:39 +0100

    Accidentally made a passing test

--- spec/airport.spec.js
+++ spec/airport.spec.js
@@ -2,13 +2,11 @@ import Airport from '../src/airport.js';
 import Plane from '../src/plane.js';
 import { assertEquals } from '../spec/test-framework.js';
 
-let airport;
-let plane;
 let actual, result;
 
 const afterEach = () => {
-    airport = undefined;
-    plane = undefined;
+    testAirport = undefined;
+    testPlane = undefined;
 }
 
 
@@ -28,6 +26,32 @@ testAirport.landPlane(testPlane);
 actual = testAirport.getPlanes().length;
 // Assert
 
+result = assertEquals(actual, expected);
+
+// Report
+console.log(result ? `Pass` : `Fail`);
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
+
+console.log(`Test 2`);
+console.log(`==================`);
+console.log(
+    `Check if the plane is already in the airport`
+);
+// Arrange
+testPlane = new Plane('EthanAir 1');
+testAirport = new Airport();
+expected = testPlane;
+
+// Act
+testAirport.landPlane(testPlane);
+
+actual = testAirport.getPlanes()[0];
+
+// Assert
 result = assertEquals(actual, expected);
 
 // Report
